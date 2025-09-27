94210596973c6190b06e33f6b737f955eb476ec6
Wilmer Zwietering <wzwietering@gmail.com>
2025-09-27 18:58:59 +0200

    Docker images

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
